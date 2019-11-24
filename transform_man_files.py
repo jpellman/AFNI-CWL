580b58374e4682375a5dc273268a21a3dd316e8f
John Pellman <pellman.john@gmail.com>
2019-11-24 11:04:01 -0500

    Update to transform_man_files.py to reduce false positives that made it through the filter.

--- transform_man_files.py
+++ transform_man_files.py
@@ -3,26 +3,36 @@ import re
 import sys
 
 path = sys.argv[1]
+form1or3 = []
+form2 = []
 with open(path,'r') as f:
     for line in f:
         # Matches forms 1 and 3 of documentation style.
-        m = re.match('^\s+-\w+.*[:=]',line)
+        m = re.match('^\s+-[A-Za-z]\w+.*[:=]',line)
         # Print match and then print all lines until next empty line.
         if m:
-            print(line, end="")
+            form1or3.append(line)
             # This sometimes iterates over other matches of the above regex.
             # In the absence of more complex logic we tolerate this.
             while line.strip():
                 line = f.readline()
-                print(line, end="")
+                form1or3.append(line)
         # Handles form 2
         else:
-            m = re.match('^\s+-\w+.*',line)
+            m = re.match('^\s+-[A-Za-z]\w+.*',line)
             if m:
-                print(line, end="")
+                form2.append(line)
                 # This sometimes iterates over other matches of the above regex.
                 # In the absence of more complex logic we tolerate this.
                 while line.strip():
                     line = f.readline()
-                    print(line, end="")
+                    form2.append(line)
+
+if form1or3:
+    for line in form1or3:
+        print(line,end="")
+elif form2:
+    for line in form2:
+        print(line,end="")
+
 
